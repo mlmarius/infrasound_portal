cd685fb2c31652b2ce5b2a42ab1a465acd84302f
Liviu Manea <mlmarius@yahoo.com>
2019-06-05 11:32:23 +0300

    typo

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -47,7 +47,7 @@
                             <a class="nav-link" href="{{@route('home')}}">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link {{ Request::is('download') ? 'active' : '' }}" href="{{@route('download_data')}}">Downlad data</a>
+                            <a class="nav-link {{ Request::is('download') ? 'active' : '' }}" href="{{@route('download_data')}}">Download data</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link {{ Request::is('memorandum') ? 'active' : '' }}" href="{{@route('memorandum')}}">Memorandum of understanding</a>
